43e3bb1555eefe0db8ec3d80ff67460abdf650a1
Manoj Waikar <manoj.waikar@intercax.com>
2015-12-10 10:43:24 +0530

    update code with comments

--- js/simplest.js
+++ js/simplest.js
@@ -1,5 +1,7 @@
 var rootObj = {};
 var pdata = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// always keep a copy of the main data
 rootObj.pdata = pdata;
 
 controlDiv = d3.select("#conrtols");
@@ -10,17 +12,20 @@ controlDiv.append("button")
 
 selectDIV = d3.select("#example1");
 
-function visualize(pdata, toUpdate) {
+function visualize(pdata) {
+  // The function passed to the d3.data function produces
+  // keys for the function. If this function is not passed
+  // then the index (0..n-1) is assumed to be the key
   var divWithData = selectDIV.selectAll("p")
     .data(pdata, function (d) { return d; });
 
+  // this is called for each data item which is not in pdata
   divWithData.exit().remove();
 
+  // new items appear which are in pdata
   divWithData.enter()
     .append("p")
-    .text(function(d) {
-        return d;
-    });
+    .text(function(d) { return d; });
 };
 
 function isEven(n) {
@@ -29,13 +34,16 @@ function isEven(n) {
 
 function filterData() {
   var numBetween1and2 = Math.floor((Math.random() * 2) + 1);
-  console.log("numBetween1and2: " + numBetween1and2);
+  var isNumEven = isEven(numBetween1and2);
+  var isNumEvenString = isNumEven ? "even" : "odd";
+  console.log("numBetween1and2:(" + numBetween1and2 + ") which is " + isNumEvenString + ", hence showing " + isNumEvenString + " data");
   
+  // filter the data from the main data
   var filteredData = rootObj.pdata.filter(function (n) {
-    return isEven(numBetween1and2) ? n % 2 === 0 : n % 2 !== 0;
+    return isNumEven ? isEven(n) : !isEven(n);
   });
   
-  visualize(filteredData, true);
+  visualize(filteredData);
 };
 
-visualize(rootObj.pdata, false);
+visualize(rootObj.pdata);
